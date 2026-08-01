fd84184c45db1acb20b586b5185d12c3c4fd4f9a
1Pythor1 <1pythor1@gmail.com>
2026-08-01 03:44:36 +0200

    fix loop too fast 2

--- cmds_core/timer_clock/timer_clock.c
+++ cmds_core/timer_clock/timer_clock.c
@@ -36,7 +36,7 @@ short wait_expression(short (*expression)(void*), void* ctx,  int duration){
     start_timer_clock(timer, duration);
 
     while(!((result = expression(ctx)) || check_timer_clock(timer)))
-        sleep_ms(10);
+        Sleep(10);
     
     free_timer_clock(timer);
     return result;
